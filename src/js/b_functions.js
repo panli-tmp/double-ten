c1f994fdc3ecfa77318d4d38bbf2873e0c95d7cc
zanjs <root@zanjs.com>
2015-09-30 11:49:12 +0800

    animation

---
+++ src/js/b_functions.js
@@ -0,0 +1,3 @@
+function Z(id){
+  return document.getElementById(id);
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,26 @@
 ;(function(){
-  
+
+  var loadingArr = [
+        'dist/images/star-bg.png',
+        'dist/images/star.png',
+        'dist/images/head-iphone6.png',
+    ];
+
+    var loadF = false;
+
+    var imgNum = 0;
+    for( var i=0;i<loadingArr.length;i++){
+        var img = document.createElement('img');
+        img.src = loadingArr[i];
+        img.onload = function(){
+            imgNum ++ ;
+            Z('load-count').style.width = Math.floor(((imgNum/loadingArr.length).toFixed(2)*100)) + '%';
+            Z('load-text').innerHTML = Math.floor(((imgNum/loadingArr.length).toFixed(2)*100))+"%";
+            if(imgNum == loadingArr.length){
+                Z('progress-bar').style.display = "none";
+                loadF = true;
+            }
+        };
+    };
 
 })();
